6e58afb1dbf43fd2798c8b229b750a9ab65afab8
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-06 18:34:27 +0530

    Update codeitmp.js

--- codeitmp.js
+++ codeitmp.js
@@ -2,17 +2,18 @@ const code1=[
     "◒","।","ऋ","॥","ऽ","ꣳ","ᳩ","ॐ","ᳪ","अ","ॽ","ई","ऐ","ऌ","इ","उ","ऊ","ए","ॠ","आ","ॡ","१","ॿ","७","६","य","४","ক","খা","ঙ"
   ];
   var seq=" "+"a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z"+"9"+"2"+"4";
+var outen;
+var outde;
 function encode(a) {
 for (let num=0; num < seq.length; num++) {
- var outen= a;
+  outen= a;
     outen=outen.replaceAll(seq[num], code1[num]);
-    alert(outen);
   } 
     return outen;
 }
 function decode(b){
   for (let num=0; num < seq.length; num++) {
-     var outde= b;
+      outde= b;
       outde=outde.replaceAll( code1[num],seq[num]);
     }
      return outde; 
