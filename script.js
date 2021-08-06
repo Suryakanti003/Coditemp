2e3e43a3b0388f3bcc35d026795ff13fc89f602c
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-06 17:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function encodet(){
+function encodet() {
 var inpv =document.getElementById("inptext").value;
    document.getElementById("out").innerHTML=encode(inpv);
 }
-function decodet(){
+function decodet() {
 var inpv2 =document.getElementById("inptext2").value;
    document.getElementById("out2").innerHTML=decode(inpv2);
 }
