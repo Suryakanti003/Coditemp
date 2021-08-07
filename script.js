40b2af794b3f67267ee648a2ad08e59e2a1f1bb6
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-07 12:23:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+var seq=" "+"a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z"+"9"+"2"+"4";
+var password="GOD IS GREAT";
+var output;
 function encodet() {
-var inpv =document.getElementById("inptext").value;
-   var outv=encode(inpv);
-   setTimeout(function(){ 
-     document.getElementById("out").innerHTML=outv;
-}, 1000);
+//var num=0;
+var inpv=document.getElementById("inptext").value;
+output=inpv;
+for (let num=0; num < seq.length; num++) {
+   output=output.replaceAll(seq[num], code1[num]);
+   //console.log(seq[num]+ code1[num]);
+  }
+setTimeout(function(){ 
+    document.getElementById("out").innerHTML=output;
+}, 100);
 }
-function decodet() {
-var inpv2 =document.getElementById("inptext2").value;
-   var outv2=decode(inpv2);
-   setTimeout(function(){ 
-    
-   document.getElementById("out2").innerHTML=outv2;
-}, 1000);
+function decodet(){
+  var inpv2=document.getElementById("inptext2").value;
+  output=inpv2;
+  for (let num=0; num < seq.length; num++) {
+     output=output.replaceAll( code1[num],seq[num]);
+     //console.log(seq[num]+ code1[num]);
+    }
+  setTimeout(function(){ 
+      document.getElementById("out2").innerHTML=output;
+  }, 100);
 }
