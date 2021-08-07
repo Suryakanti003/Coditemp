db424d882f8e4f467c9e8571c17acfd5dd487489
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-07 12:15:43 +0530

    Update codeitmp.js

--- codeitmp.js
+++ codeitmp.js
@@ -4,15 +4,18 @@ const code1=[
   var seq=" "+"a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z"+"9"+"2"+"4";
 var outen;
 var outde;
+var num;
 function encode(a) {
-for (let num=0; num < seq.length; num++) {
+    num=0;
+for (num=0; num < seq.length; num++) {
   outen= a;
     outen=outen.replaceAll(seq[num], code1[num]);
   } 
     return outen;
 }
 function decode(b){
-  for (let num=0; num < seq.length; num++) {
+    num=0;
+  for (num=0; num < seq.length; num++) {
       outde= b;
       outde=outde.replaceAll( code1[num],seq[num]);
     }
