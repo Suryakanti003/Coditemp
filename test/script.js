b934ed2fc514ac5cd58e26a5da9842caf09821df
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-01 16:27:58 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -2,7 +2,9 @@ function encodet(){
   var input = document.getElementById("inptext").value;
   var output ="";
   for (var i = 0; i < input.length; i++){
-    output += input[i].charCodeAt(0).toString(10)+1;
+    if (i>=input.length){
     document.getElementById("out").innerHTML=output;
+    }
+   parseInt( output += input[i].charCodeAt(0).toString(10))+1;
   }
 }
