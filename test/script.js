601de0f264af7c1f45f81bce57705edaf9cab250
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-01 18:21:33 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -17,3 +17,41 @@ function encodet(){
    document.getElementById("out").innerHTML=final;
   }, 200);
 }
+function decodet(){
+
+  var input = document.getElementById("inptext2").value;
+
+  var output ="";
+
+  var code1="";
+
+  var final="";
+
+  var fo="";
+
+  for (var i = 0; i < input.length; i++){
+
+    code1=input[i].charCodeAt(0).toString(10);
+
+ output =parseInt(code1)+1;
+
+  final+=String.fromCodePoint(output);
+
+}
+
+/*  setTimeout(function(){ 
+
+     for (var i = 0; i < final.length; i++){
+
+     fo+= String.fromCodePoint(final[i]);
+
+     }  }, 150);*/
+
+   setTimeout(function(){ 
+
+   document.getElementById("out2").innerHTML=final;
+
+  }, 200);
+
+}
+
