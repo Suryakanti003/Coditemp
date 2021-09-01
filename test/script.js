d0814238e986e73c60c176f896b6da4211c1c66b
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-01 17:23:26 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,10 +1,19 @@
 function encodet(){
   var input = document.getElementById("inptext").value;
   var output ="";
+  var code1="";
+  var final="";
+  var fo="";
   for (var i = 0; i < input.length; i++){
-    if (i=input.length){
-    document.getElementById("out").innerHTML=output;
-    }
-   output += ( parseInt(input[i].charCodeAt(0).toString(10))+1);
-  }
+    code1=input[i].charCodeAt(0).toString(10);
+ output =parseInt(code1)+1;
+  final+=String.fromCodePoint(output);
+}
+/*  setTimeout(function(){ 
+     for (var i = 0; i < final.length; i++){
+     fo+= String.fromCodePoint(final[i]);
+     }  }, 150);*/
+   setTimeout(function(){ 
+   document.getElementById("out").innerHTML=final;
+  }, 200);
 }
