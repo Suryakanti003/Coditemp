13f22fb69648bad0aaf0ef83fc3b4833489cf373
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-02 21:13:05 +0530

    Update codeitmp.js

--- codeitmp.js
+++ codeitmp.js
@@ -1,28 +1,30 @@
 function encode(e){
-
-  let input = e;
-
-  let output ="";
-
-  let code1="";
-
-  let final="";
-
-  let fo="";
-
-  for (var i = 0; i < input.length; i++){
-
-    code1=input[i].charCodeAt(0).toString(10);
-
- output =parseInt(code1)-1;
-
-  final+=String.fromCodePoint(output);
-      if (i>=input.length){
-      return final;
-      }
-
+let input = e;
+let output ="";
+let code1="";
+let final="";
+let fo="";
+for (var i = 0; i < input.length; i++){
+code1=input[i].charCodeAt(0).toString(10);
+output =parseInt(code1)-1;
+final+=String.fromCodePoint(output);
+if (i>=input.length){
+return final;
 }
-
-
-
+}
+}
+function decode(e){
+let input = e;
+let output ="";
+let code1="";
+let final="";
+let fo="";
+for (var i = 0; i < input.length; i++){
+code1=input[i].charCodeAt(0).toString(10);
+output =parseInt(code1)+1;
+final+=String.fromCodePoint(output);
+if (i>=input.length){
+return final;
+}
+}
 }
