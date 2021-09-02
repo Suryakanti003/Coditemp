a8b02a3244d60718f6b09cd4009d06296f9f33d1
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-02 20:11:51 +0530

    Update codeitmp.js

--- codeitmp.js
+++ codeitmp.js
@@ -1,25 +1,28 @@
-const code1=[
-    "◒","।","ऋ","॥","ऽ","ꣳ","ᳩ","ॐ","ᳪ","अ","ॽ","ई","ऐ","ऌ","इ","उ","ऊ","ए","ॠ","आ","ॡ","१","ॿ","७","६","य","४","ক","খা","ঙ"
-  ];
-  var seq=" "+"a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z"+"9"+"2"+"4";
-var outen;
-var outde;
-var num;
-function encode(a) {
-    num=0;
-for (num=0; num < seq.length; num++) {
-  outen= a;
-    outen=a.replaceAll(seq[num], code1[num]);
-    console.log(outen+seq[num]+code1[num]);
-  } 
-    return outen;
+function encode(e){
+
+  let input = e;
+
+  let output ="";
+
+  let code1="";
+
+  let final="";
+
+  let fo="";
+
+  for (var i = 0; i < input.length; i++){
+
+    code1=input[i].charCodeAt(0).toString(10);
+
+ output =parseInt(code1)-1;
+
+  final+=String.fromCodePoint(output);
+      if (i>=input.length){
+      return final;
+      }
+
 }
-/*function decode(b){
-    num=0;
-  for (num=0; num < seq.length; num++) {
-      outde= b;
-      outde=outde.replaceAll( code1[num],seq[num]);
-    }
-     return outde; 
+
+
+
 }
-*/
