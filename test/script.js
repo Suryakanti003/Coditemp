28ff04f18f2f993be64dddafec9345c0c2aace61
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-21 21:36:23 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -7,8 +7,17 @@ function encodet(){
   var code1="";
   var final="";
   var fo="";
+  for (var i = 0; i < input.length; i++){
+
+    code1=input[i].charCodeAt(0).toString(10);
+
+ output =parseInt(code1)+1;
+
+  final+=String.fromCodePoint(output);
+
+}
    setTimeout(function(){ 
-   document.getElementById("out").innerHTML=rncoded;
+   document.getElementById("out").innerHTML=final;
   }, 200);
 }
 function decodet(){
