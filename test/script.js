028ffb568e83c6dac2f9aa8da75c53f6f64cfae7
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-09-21 21:29:07 +0530

    Clear clutter from beta

--- test/script.js
+++ test/script.js
@@ -1,20 +1,12 @@
-var rncoded=encode("abcd");
+
 function encodet(){
   
   var input = document.getElementById("inptext").value;
+  rencoded=encode(input);
   var output ="";
   var code1="";
   var final="";
   var fo="";
-  for (var i = 0; i < input.length; i++){
-    code1=input[i].charCodeAt(0).toString(10);
- output =parseInt(code1)+1;
-  final+=String.fromCodePoint(output);
-}
-/*  setTimeout(function(){ 
-     for (var i = 0; i < final.length; i++){
-     fo+= String.fromCodePoint(final[i]);
-     }  }, 150);*/
    setTimeout(function(){ 
    document.getElementById("out").innerHTML=rncoded;
   }, 200);
