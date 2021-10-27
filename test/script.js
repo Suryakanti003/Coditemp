f66e514c3de83b62b51ab7613f1bd158c0ce1c65
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-10-27 11:32:58 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,26 +1,37 @@
 function encodet(){
+  document.getElementById("button").innerHTML="You can double click to copy";
   var encoding=document.getElementById("encoding").value;
+  var encoding2=document.getElementById("encoding2").value;
   var input = document.getElementById("inptext").value;
   var output ="";
   var code1="";
   var final="";
   encoding=parseInt(encoding);
+  encoding2=parseInt(encoding2);
   for (var i = 0; i < input.length; i++){
 
     code1=input[i].charCodeAt(0).toString(10);
-
+    if (i % 2 == 0) {
  output =parseInt(code1)+encoding;
-
+    } else {
+      output =parseInt(code1)+(21-encoding2);
+    }
   final+=String.fromCodePoint(output);
 
 }
    setTimeout(function(){ 
-   document.getElementById("out").innerHTML=final;
+   document.getElementById("out").value=final;
   }, 200);
+  setTimeout(function(){ 
+    document.getElementById("button").innerHTML="encode"; 
+  },4000);
 }
 function decodet(){
+  document.getElementById("button2").innerHTML="You can double click to copy";
   var encoding=document.getElementById("encoding").value;
+  var encoding2=document.getElementById("encoding2").value;
   encoding=parseInt(encoding);
+  encoding2=parseInt(encoding2);
   var input = document.getElementById("inptext2").value;
 
   var output ="";
@@ -34,38 +45,39 @@ function decodet(){
   for (var i = 0; i < input.length; i++){
 
     code1=input[i].charCodeAt(0).toString(10);
-
+    if (i % 2 == 0) {
  output =parseInt(code1)-encoding;
+    } else {
+      output =parseInt(code1)-(21-encoding2);
+    }
 
   final+=String.fromCodePoint(output);
 
 }
 
-/*  setTimeout(function(){ 
-     for (var i = 0; i < final.length; i++){
-     fo+= String.fromCodePoint(final[i]);
-     }  }, 150);*/
 
    setTimeout(function(){ 
 
-   document.getElementById("out2").innerHTML=final;
+   document.getElementById("out2").value=final;
 
   }, 200);
+  setTimeout(function(){ 
+    document.getElementById("button2").innerHTML="decode"; 
+  },4000);
 
 }
-var input1 = document.getElementById("myfile");
-var output1 = document.getElementById("output");
-var fileE;
-
-input1.addEventListener("change", function () {
-  if (this.files && this.files[0]) {
-    var myFile = this.files[0];
-    var reader = new FileReader();
-    
-    reader.addEventListener('load', function (e) {
-  document.getElementById("out2").innerHTML= e.target.result;
-    });
-    
-    reader.readAsBinaryString(myFile);
-  }   
-});
+function copyen(){
+  var copyText = document.getElementById("out");
+  copyText.select();
+  document.getElementById("button").innerHTML="encode";
+  document.execCommand("copy");
+   
+}
+function copyde(){
+  var copyText = document.getElementById("out2");
+  copyText.select();
+  document.getElementById("button2").innerHTML="decode"; 
+  document.execCommand("copy");
+ 
+ 
+}
