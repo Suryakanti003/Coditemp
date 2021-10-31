21cde6e62c556d675ff46f0961b7d415f57be9ec
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-10-31 12:55:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,90 @@
-var seq=" "+"a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z"+"A"+"B"+"C"+
-"D"+"E"+"F"+"G"+"H"+"I"+"J"+"K"+"L"+"M"+"N"+"O"+"P"+"Q"+"S"+"T"+"U"+"V"+"W"+"X"+"Y"+"Z"+"1"+"2"+"3"+"4"+"5"+"6"+"7"+"8"+"9"+"0"+
-"!"+"@"+"#"+"$"+"%"+"^"+"&"+"*"+"("+")"+"-"+"="+"_"+"+"+"~"+"`"+","+"."+"/"+"?"+";"+":"+"<"+">"+"["+"]"+"{"+"}";
-const code1=
-  "◓"+"।"+"॰"+"ꣲ"+"ॽ"+"॥"+"ऽ"+"ꣳ"+"ᳩ"+"ॐ"+"ᳪ"+"अ"+"ई"+"ऋ"+"ॡ"+"आ"+"उ"+"ॠ"+"ए"+"इ"+"ऊ"+"ऌ"+"ऐ"+"ओ"+"ॶ"+"ऒ"+"ॲ"+"ऄ"+"औ"+
-  "ॵ"+"ऍ"+"ॴ"+"ऑ"+"ऎ"+"क"+"ख़"+"ॻ"+"च"+"ज़"+"झ"+"ठ"+"ॾ"+"ढ"+"ख"+"ग़"+"ङ"+"ज"+"ॼ"+"ट"+"ड़"+"ढ़"+"ण"+"द"+"ऩ"+"फ़"+"भ"+"य़"+"ऱ"+
-  "ऴ"+"त"+"ध"+"प"+"ब"+"म"+"ॺ"+"ल"+"व"+"थ"+"न"+"फ"+"ॿ"+"य"+"र"+"ळ"+"श"+"ष"+"०"+"३"+"६"+"९"+"स"+"१"+"४"+"७"+"ह"+"२"+"५"+"८";
-var output;
-function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-     dummy.style.opacity = "0";
-    document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+function encodet(){
+  document.getElementById("button").innerHTML="You can double click to copy";
+  var encoding=document.getElementById("encoding").value;
+  var encoding2=document.getElementById("encoding2").value;
+  var input = document.getElementById("inptext").value;
+  var output ="";
+  var code1="";
+  var final="";
+  encoding=parseInt(encoding);
+  encoding2=parseInt(encoding2);
+  for (var i = 0; i < input.length; i++){
+
+    code1=input[i].charCodeAt(0).toString(10);
+    if (i % 2 == 0) {
+ output =parseInt(code1)+encoding;
+    } else {
+      output =parseInt(code1)+(21-encoding2);
+    }
+  final+=String.fromCodePoint(output);
+
 }
-function encodet() {
-//var num=0;
-var inpv=document.getElementById("inptext").value;
-output=inpv;
-for (let num=0; num < seq.length; num++) {
-   output=output.replaceAll(seq[num], code1[num]);
-   //console.log(seq[num]+ code1[num]);
-  }
-setTimeout(function(){ 
-    document.getElementById("out").innerHTML=output;
-}, 100);
+   setTimeout(function(){ 
+   document.getElementById("out").value=final;
+  }, 200);
+  setTimeout(function(){ 
+    document.getElementById("button").innerHTML="encode"; 
+  },4000);
 }
 function decodet(){
-  var inpv2=document.getElementById("inptext2").value;
-  output=inpv2;
-  for (let num=0; num < seq.length; num++) {
-     output=output.replaceAll( code1[num],seq[num]);
-     //console.log(seq[num]+ code1[num]);
+  document.getElementById("button2").innerHTML="You can double click to copy";
+  var encoding=document.getElementById("encoding").value;
+  var encoding2=document.getElementById("encoding2").value;
+  encoding=parseInt(encoding);
+  encoding2=parseInt(encoding2);
+  var input = document.getElementById("inptext2").value;
+
+  var output ="";
+
+  var code1="";
+
+  var final="";
+
+  var fo="";
+
+  for (var i = 0; i < input.length; i++){
+
+    code1=input[i].charCodeAt(0).toString(10);
+    if (i % 2 == 0) {
+ output =parseInt(code1)-encoding;
+    } else {
+      output =parseInt(code1)-(21-encoding2);
     }
-  setTimeout(function(){ 
-      document.getElementById("out2").innerHTML=output;
-  }, 100);
+
+  final+=String.fromCodePoint(output);
+
 }
-document.getElementsByTagName("h1")[0].ondblclick = function() {
- rotate();
-};
+
+
+   setTimeout(function(){ 
+
+   document.getElementById("out2").value=final;
+
+  }, 200);
+  setTimeout(function(){ 
+    document.getElementById("button2").innerHTML="decode"; 
+  },4000);
+
+}
 function copyen(){
-  copyToClipboard(document.getElementById("out").innerHTML);
+  var copyText = document.getElementById("out");
+  copyText.select();
+  document.getElementById("button").innerHTML="encode";
+  document.execCommand("copy");
+   
 }
 function copyde(){
-  copyToClipboard(document.getElementById("out2").innerHTML);
+  var copyText = document.getElementById("out2");
+  copyText.select();
+  document.getElementById("button2").innerHTML="decode"; 
+  document.execCommand("copy");
+ 
+ 
+}
+function textAreaAdjust(element) {
+
+  element.style.height = "1px";
+
+  element.style.height = (5+element.scrollHeight)+"px";
+
 }
