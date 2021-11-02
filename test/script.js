ab8084099b147111273731408496da14f56a73a3
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-11-02 12:04:29 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,20 +1,51 @@
 function encodet(){
   document.getElementById("button").innerHTML="You can double click to copy";
-  var encoding=document.getElementById("encoding").value;
-  var encoding2=document.getElementById("encoding2").value;
+  var encoding=(document.getElementById("encoding").value);
   var input = document.getElementById("inptext").value;
   var output ="";
   var code1="";
   var final="";
-  encoding=parseInt(encoding);
-  encoding2=parseInt(encoding2);
+  var ifu=parseInt(encoding.charAt(0));
+  if(ifu=1) {
+    console.log("abcd");
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(3)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(1)));
+  } else if(ifu=2) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(3)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=3) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=4) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(3)));
+  }else if(ifu=5) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+    var encoding2 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=6) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(3)));
+  }else if(ifu=7) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(4)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=8) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(4)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=9) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=0) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(4)));
+  }
+ 
   for (var i = 0; i < input.length; i++){
 
     code1=input[i].charCodeAt(0).toString(10);
     if (i % 2 == 0) {
- output =parseInt(code1)+encoding;
+ output =parseInt(code1)+encoding1;
     } else {
-      output =parseInt(code1)+(21-encoding2);
+      output =parseInt(code1)+(100-encoding2);
     }
   final+=String.fromCodePoint(output);
 
@@ -29,9 +60,6 @@ function encodet(){
 function decodet(){
   document.getElementById("button2").innerHTML="You can double click to copy";
   var encoding=document.getElementById("encoding").value;
-  var encoding2=document.getElementById("encoding2").value;
-  encoding=parseInt(encoding);
-  encoding2=parseInt(encoding2);
   var input = document.getElementById("inptext2").value;
 
   var output ="";
@@ -39,16 +67,46 @@ function decodet(){
   var code1="";
 
   var final="";
-
-  var fo="";
-
+  var ifu=parseInt(encoding.charAt(0));
+  if(ifu=1) {
+    console.log("abcd");
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(3)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(1)));
+  } else if(ifu=2) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(3)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=3) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=4) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(3)));
+  }else if(ifu=5) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+    var encoding2 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=6) {
+    var encoding1 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(3)));
+  }else if(ifu=7) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(4)));
+    var encoding2 = parseInt(encoding.charAt(2))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=8) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(4)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(2)));
+  }else if(ifu=9) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(4))*10+(parseInt(encoding.charAt(1)));
+  }else if(ifu=0) {
+    var encoding1 = parseInt(encoding.charAt(3))*10+(parseInt(encoding.charAt(2)));
+    var encoding2 = parseInt(encoding.charAt(1))*10+(parseInt(encoding.charAt(4)));
+  }
   for (var i = 0; i < input.length; i++){
 
     code1=input[i].charCodeAt(0).toString(10);
     if (i % 2 == 0) {
- output =parseInt(code1)-encoding;
+ output =parseInt(code1)-encoding1;
     } else {
-      output =parseInt(code1)-(21-encoding2);
+      output =parseInt(code1)-(100-encoding2);
     }
 
   final+=String.fromCodePoint(output);
@@ -80,11 +138,4 @@ function copyde(){
   document.execCommand("copy");
  
  
-}
-function textAreaAdjust(element) {
-
-  element.style.height = "1px";
-
-  element.style.height = (5+element.scrollHeight)+"px";
-
 }
